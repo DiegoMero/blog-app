64e834508a28d0211e77ef2257103ca18276a324
Diego Mero <diegomero_1999@hotmail.com>
2022-11-09 00:14:37 -0500

    Use models to insert data

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :users, foreign_key: "authorId"
-  belongs_to :posts, foreign_key: "postId"
+  belongs_to :author, class_name: 'User', foreign_key: 'authorId'
+  belongs_to :post, foreign_key: 'postId'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :users, foreign_key: "authorId"
+  belongs_to :author, class_name: 'User', foreign_key: 'authorId'
   has_many :likes
   has_many :comments
 end
