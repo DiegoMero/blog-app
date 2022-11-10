2647d711fe4033b6f504eecedbadb2016c4b8721
Diego Mero <diegomero_1999@hotmail.com>
2022-11-10 03:48:15 -0500

    Fix linter errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: "Diegolo", photo: 'link', bio: 'Nice guy', postsCounter: 5) }
+  subject { User.new(name: 'Diegolo', photo: 'link', bio: 'Nice guy', postsCounter: 5) }
 
   before { subject.save }
 
