97ea17472a7dab32fbfdfce9f226312b2bc5b9f7
Diego Mero <diegomero_1999@hotmail.com>
2022-11-10 03:07:36 -0500

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
+  validates :name, presence: true
+  validates :postsCounter, numericality: { greater_than_or_equal_to: 0 }
+
   def three_most_recent_posts
     Post.where(authorId: id).last(3)
   end
-
-  
 end
