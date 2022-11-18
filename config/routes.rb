c1fb5cb7fa6694d32a0e85d76a42c81d69fd1108
Diego Mero <diegomero_1999@hotmail.com>
2022-11-18 12:03:05 -0500

    Add form to create a comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only [:create]
-      resources :likes, only [:create]
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
 end
