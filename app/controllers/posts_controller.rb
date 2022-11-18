9a427a07ed1af0b5a935b33308db98ce40e3add9
Diego Mero <diegomero_1999@hotmail.com>
2022-11-18 00:41:44 -0500

    Add html and css for posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(authorId: params[:user_id])
+    @users = User.all.order(id: :asc)
+  end
 
   def show; end
 end
