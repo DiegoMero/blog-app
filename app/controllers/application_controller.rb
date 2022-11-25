abb122e5b36fb10e9642a54c716a02bf18b65d5a
Diego Mero <diegomero_1999@hotmail.com>
2022-11-25 16:54:21 -0500

    Add routes and new migrations sheema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  # add_flash_types :danger, :info, :warning, :success, :messages
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+
+  # protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+  # en
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,11 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_143003) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
